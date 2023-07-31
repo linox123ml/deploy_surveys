87a8d49873693430a47a14bc45c26f6948dab988
Linox <macove.polo@gmail.com>
2023-07-31 10:49:25 -0500

    a

---
+++ assets/use-tree-walker-82ad570f.js
@@ -0,0 +1,1 @@
+import{m as f}from"./use-outside-click-ee90a11d.js";import{w as d}from"./index-80717ac9.js";function m({container:e,accept:r,walk:n,enabled:t}){d(()=>{let a=e.value;if(!a||t!==void 0&&!t.value)return;let c=f(e);if(!c)return;let l=Object.assign(i=>r(i),{acceptNode:r}),o=c.createTreeWalker(a,NodeFilter.SHOW_ELEMENT,l,!1);for(;o.nextNode();)n(o.currentNode)})}export{m as p};
