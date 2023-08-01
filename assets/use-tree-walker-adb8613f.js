aaed1393803117de8be25d96a46d754c04a374e4
Linox <macove.polo@gmail.com>
2023-08-01 14:34:50 -0500

    Correo personal activado

--- assets/use-tree-walker-adb8613f.js
+++
@@ -1,1 +0,0 @@
-import{m as f}from"./use-outside-click-2341a81b.js";import{w as d}from"./index-e822e5e2.js";function m({container:e,accept:r,walk:n,enabled:t}){d(()=>{let a=e.value;if(!a||t!==void 0&&!t.value)return;let c=f(e);if(!c)return;let l=Object.assign(i=>r(i),{acceptNode:r}),o=c.createTreeWalker(a,NodeFilter.SHOW_ELEMENT,l,!1);for(;o.nextNode();)n(o.currentNode)})}export{m as p};
