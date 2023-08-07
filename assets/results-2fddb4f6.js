979a386a3c007e249859ed4bfc73d0f18f7a7f51
Linox <macove.polo@gmail.com>
2023-08-07 17:58:23 -0500

    axios

--- assets/results-2fddb4f6.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{c as r,o}from"./index-f5b1306e.js";const t={};function c(s,n){return o(),r("h1",null,"resultados")}const l=e(t,[["render",c]]);export{l as default};
