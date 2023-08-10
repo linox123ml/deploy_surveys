ea39c431b3841f6195f947320c9fa9562cad8901
Linox <macove.polo@gmail.com>
2023-08-10 15:17:22 -0500

    Actualizacion de datos automatico - Amnistia

--- assets/index-27e75f0c.js
+++
@@ -1,1 +0,0 @@
-import{s as o,g as r,o as t}from"./index-dfb52d5f.js";const a={__name:"index",setup(n){return(s,_)=>{const e=o("router-view");return t(),r(e)}}};export{a as default};
---
+++ assets/index-fc40e758.js
@@ -0,0 +1,1 @@
+import{s as o,g as r,o as t}from"./index-d6363c5a.js";const a={__name:"index",setup(n){return(s,_)=>{const e=o("router-view");return t(),r(e)}}};export{a as default};
